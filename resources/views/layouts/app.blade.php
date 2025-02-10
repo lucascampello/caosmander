4c514bd6f1395e32647740fa2a06f840bae835cc
Lucas Campello da Pieva <lucas.campello@gmail.com>
2025-02-09 23:41:47 -0300

    Adição dos 10 Planos

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Div Centralizada</title>
+    <link rel="shortcut icon" href="{{asset('assets/images/icon.png')}}" type="image/png">
     <link href="{{asset('assets/bootstrap/bootstrap.min.css')}}" rel="stylesheet">
     <link href="{{asset('assets/css/app.css')}}" rel="stylesheet">
     @yield('styles')
